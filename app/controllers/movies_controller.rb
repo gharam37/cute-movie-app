7fe6f42aee5a271d424ed7978231e2cf3475ba51
romy <romy.zakaria@gmail.com>
2024-02-29 00:42:09 +0200

    show movies by average rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.includes(:reviews).order(:title)
-
+    # @movies = Movie.includes(:reviews).order(:title)
+    @movies = Movie.joins(:reviews).select('movies.title as title , movies.id as movie_id, avg(reviews.rating) as average_rating').group(:movie_id).order('average_rating DESC')
   end
 end
