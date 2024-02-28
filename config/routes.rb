df18093cd3c737482b848a94657a1224c8ec001d
romy <romy.zakaria@gmail.com>
2024-02-29 00:57:39 +0200

    feat: route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'movies/index'
   get 'staffs/index'
   post 'staffs/search'
+  get 'staffs/search'
+
+   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
